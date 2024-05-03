b6c25fb902b3bcb13549d176bae87e4013c4f92d
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-05-04 00:12:25 +0200

    fix-word-censured

--- paragraph.php
+++ paragraph.php
@@ -1,23 +1,28 @@
-<?php
-$paragrafo= $_GET['paragraph'];
-$censura = $_GET['censure'];
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>paragraph</title>
+    <title>Paragraph Processing</title>
 </head>
 <body>
-     
-    <p>
-        <b><?php echo $paragrafo; ?></b>   <?php echo strlen($paragrafo); ?>           
-    </p>
-    
-    <p>
-        <b><?php echo str_replace($censura, '***', $paragrafo); ?></b> <?php echo strlen($paragrafo); ?>
-    </p>
+
+<?php
+// otteniamo i dati con GET
+$paragraph = $_GET['paragraph'] ?? '';
+$censorWord = $_GET['censure'] ?? '';
+
+// STAMPIAMO CON ECHO I PARAGRAFI
+echo "<p>Paragrafo: $paragraph</p>";
+echo "<p>Lunghezza: " . strlen($paragraph) . "</p>";
+
+// SOSTITUIAMO LE PAROLE CENSURATE CON ***
+$censoredParagraph = str_ireplace($censorWord, '***', $paragraph);
+
+// STAMPIAMO DI NUOVO IL PARAGRAFH CON LA SUA LUNGHEZZA E LE PAROLE CENSURATE E LA SUA LUNGHEZZA
+echo "<p>Paragrafo Censurato: $censoredParagraph</p>";
+echo "<p>Lunghezza: " . strlen($censoredParagraph) . "</p>";
+?>
+
 </body>
 </html>
